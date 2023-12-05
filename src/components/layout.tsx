b5e60e87bf847598db2a186adde8ce2e88afa075
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2023-12-06 05:05:31 +0700

    feat: initial commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -67,8 +67,24 @@ const Layout = () => {
       </div>
 
       {/* form comment */}
-      <div>
-        <p className="w-80 text-base font-light py-2">View more comments</p>
+      <div className="grid grid-cols-6 bg-lime-400">
+        <div className="col-start-2 col-span-5 bg-orange-400">
+          <p className="w-80 text-base font-light py-2">View more comments</p>
+        </div>
+        <div className="col-start-2 bg-teal-400">
+          <div className="flex justify-end pr-3">
+            <Avatar>
+              <AvatarImage src="#" />
+              <AvatarFallback>JB</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+        <div className="col-span-4 bg-orange-400">
+          <h4 className="text-base font-bold">Jude Bellinghem</h4>
+          <p className="w-80 text-base font-light py-2">Hola Madridistas! It is the proudest day of my life to join the greatest club in the history of the game. I will give absolutely everything I have to help this team win. Thank you for the amazing 
+          welcome. HALA MADRID!!!</p>
+          <img src="/src/assets/post-image.png" alt="post-image" className="pb-5"/>
+        </div>
       </div>
       <div className="pt-5">
         <Footer />
